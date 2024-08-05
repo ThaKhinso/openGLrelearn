f2e9c4dc09cdccd77e2d66de8ac5ae7f68c5ce15
ThaKhinso <usersoe123@gmail.com>
2024-08-05 23:40:09 +0630

    no.4 finished

--- relearn.cpp
+++ relearn.cpp
@@ -18,17 +18,29 @@ int main(void) {
 
   glfwMakeContextCurrent(window);
   
-  glewInit();
+  if(glewInit() != GLEW_OK){
+	  std::cout << "Error!\n";
+  }
+
+  std::cout << glGetString(GL_VERSION) << "\n";
+   
+  float positions[6] = {
+	  -0.5f, -0.5f,
+	   0.0f,  0.5f,
+	   0.5f, -0.5f
+  };
+
+  unsigned int buffer; 
+  glGenBuffers(1, &buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, buffer);
+  glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+  
+
 
   while(!glfwWindowShouldClose(window)) {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glBegin(GL_TRIANGLES);
-      glVertex2f(-0.5f , -0.5f);
-      glVertex2f( 0.0f ,  0.5f);
-      glVertex2f( 0.5f , -0.5f);
-    glEnd();
-
+    glDrawArrays(GL_TRIANGLES, 0, 3); 
     glfwSwapBuffers(window);
     glfwPollEvents();
   
